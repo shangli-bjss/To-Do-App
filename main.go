1d789dd3c4a8d445776c25380b042289b67fbe4b
Shang <shang.li@bjss.com>
2024-08-13 11:39:59 +0100

    added read json func

--- main.go
+++ main.go
@@ -37,27 +37,61 @@ func writeToJsonFile(filename string, todoList []ToDo) error {
 }
 
 
+func readFromJsonFile(filename string) ([]ToDo, error) {
+	var todoList []ToDo
+	file, error := os.Open(filename)
+	if error != nil {
+		return nil, error
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+
+	err := decoder.Decode(&todoList)
+	if err != nil {
+		return nil, err
+	}
+
+	return todoList, nil
+}
+
+func displayTodoList(todoList []ToDo) {
+	for i, todo := range todoList {
+		pl(i+1, ": ", todo.Item, " - ", todo.Status)
+	}
+}
 
 func main() {
-	todoList := createTodoList(
-	ToDo{"Buy groceries", "Pending"},
-	ToDo{"Clean the house", "In Progress"},
-	ToDo{"Write blog post", "Completed"},
-	ToDo{"Finish project", "Pending"},
-	ToDo{"Call mom", "Completed"},
-	ToDo{"Attend meeting", "Pending"},
-	ToDo{"Read book", "In Progress"},
-	ToDo{"Exercise", "Pending"},
-	ToDo{"Plan vacation", "Completed"},
-	ToDo{"Pay bills", "In Progress"},
-)
+	todoList, err := readFromJsonFile("todo_list.json")
+	if err != nil {
+		pl("Error reading from JSON file", err)
+		return
+	}
 
-err := writeToJsonFile("todo_list.json", todoList)
-if err != nil {
-	pl("Error writing to JSON file", err)
-	return
+	displayTodoList(todoList)
 }
 
-pl("ToDo list has been written to todo_list.json successfully")
 
-}
+// func main() {
+// 	todoList := createTodoList(
+// 	ToDo{"Buy groceries", "Pending"},
+// 	ToDo{"Clean the house", "In Progress"},
+// 	ToDo{"Write blog post", "Completed"},
+// 	ToDo{"Finish project", "Pending"},
+// 	ToDo{"Call mom", "Completed"},
+// 	ToDo{"Attend meeting", "Pending"},
+// 	ToDo{"Read book", "In Progress"},
+// 	ToDo{"Exercise", "Pending"},
+// 	ToDo{"Plan vacation", "Completed"},
+// 	ToDo{"Pay bills", "In Progress"},
+// )
+
+// err := writeToJsonFile("todo_list.json", todoList)
+// if err != nil {
+// 	pl("Error writing to JSON file", err)
+// 	return
+// }
+
+// pl("ToDo list has been written to todo_list.json successfully")
+
+// }
